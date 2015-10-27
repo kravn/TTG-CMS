3438d658635a92b453849517df718194411ee3db
Karlmarx Fabe <administrator@Macintosh.local>
2015-10-27 10:16:30 +0800

    Added carousel resources

---
+++ app/Forms/CarouselsForm.php
@@ -0,0 +1,11 @@
+<?php namespace App\Forms;
+
+use Kris\LaravelFormBuilder\Form;
+
+class CarouselsForm extends Form
+{
+    public function buildForm()
+    {
+        // Add fields here...
+    }
+}
---
+++ database/migrations/2015_10_27_021425_create_carousels_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCarouselsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('carousels', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('language_id');
+            $table->foreign('language_id')->references('id')->on('languages');
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('carousels');
+    }
+}
