2a81115326fde14815aaa721111407bb37ef9e00
Karlmarx Fabe <kmarxfabe@gmail.com>
2015-11-03 11:53:08 +0800

    Nothing

--- database/seeds/ArticleTableSeeder.php
+++ database/seeds/ArticleTableSeeder.php
@@ -10,7 +10,7 @@ class ArticleTableSeeder extends Seeder
     public function run()
     {
         DB::table('articles')->delete();
-        TestDummy::times(40)->create('App\Article');
+        TestDummy::times(20)->create('App\Article');
     }
 
 }
